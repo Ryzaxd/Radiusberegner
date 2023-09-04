297126d857cbefef659ba1dc7a45ef8bfb9d8d04
Ryzaxd <nick986h@edu.zealand.dk>
2023-09-04 09:55:41 +0200

    Listing 33.1 Radiusberegner med GUI

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.radiusberegner {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.radiusberegner to javafx.fxml;
+    exports com.example.radiusberegner;
+}
